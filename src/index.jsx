76603e959b5d4f0e2b1d751ff8cf47a1ac09fe7e
diana spieser <dianaspieser@gmail.com>
2023-02-14 00:59:57 +0100

    versioning problem resolved by downgrading redux & react

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// external modules
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, combineReducers } from "redux";
 
-import '../assets/stylesheets/application.scss';
+// internal modules
+import "../assets/stylesheets/application.scss";
+import App from "./components/app";
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
+// Reducers
+import flatsReducer from "./reducers/flats_reducer";
+import selectedFlatReducer from "./reducers/selected_flat_reducer";
 
-const root = document.getElementById('root');
-if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
-}
+const reducers = combineReducers({
+  flats: flatsReducer,
+  selectedFlat: selectedFlatReducer
+});
+
+// render an instance of the component in the DOM
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
